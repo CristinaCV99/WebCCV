c5b56a9dd0225fd956d838d191a6733ccc5e0856
Cristina Castillo <cris_rockarnolla2299@hotmail.com>
2020-05-16 23:44:44 -0500

    FINAL

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Project
 # Register your models here.
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'update')
+admin.site.register(Project, ProjectAdmin)
 
